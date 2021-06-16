0ace25124af25a92cbb14f6c78ca330cbdcb2317
Vũ Tuấn <kurobakaito146@gmail.com>
2021-06-16 16:52:38 +0700

    update update.ejs & bill_input.ts

--- src/controllers/bill_input/bill_input.ts
+++ src/controllers/bill_input/bill_input.ts
@@ -2,56 +2,28 @@ import express, { Request, Response } from "express";
 import { MySQLDb } from "../../db/db";
 import { hashPassword } from "../../libs/hash";
 
-export const bill_input = (req: Request<{}, {}, { username: string, password: string }>, res: Response) => {
+type bill = {
+    msHD: string,
+    msPhong: string,
+    NgayLapHD: Date,
+    NguoiLapHD: string,
+    Sodien: number,
+    Luongnuoc: number,
+    TienDien: number,
+    TienNuoc: number,
+    TongTien: number,
+    NgayNop: Date,
+    Nguoinop: string
+}
+
+export const bill_input = (req: Request<{}, {}, bill>, res: Response) => {
     //TODO
     const mysql = MySQLDb.getInstance();
     const db = mysql.db;
 
-    let username = req.body.username;
-    let password = req.body.password;
-
-    db.query(`SELECT * FROM users WHERE username = "${username}" AND password = "${password}"`,function(err, results) {
+    db.query(`INSERT INTO hoadon SET ?`,req.body, function (err, results) {
         if (err) throw err;
 
-        if (results.length === 0) {
-            res.render("login",{validate: "Tài khoản không tồn tại hoặc bạn nhập sai mật khẩu, tài khoản"});
-            return;
-        }
-
-
-        res.cookie('username', 'admin', { expires: new Date(Date.now() + 9000000000) });
-        res.redirect("/");
+        res.render("bill_input", { result: "Nhập hoá đơn thành công!" });
     });
-
-
-    // db.query('SELECT username, password FROM users', function (err, results) {
-    //     if (err) throw err;
-    //     const user = results;
-
-    //     var count1 = 0;
-    //     var count2 = 0;
-
-    //     for (var ojb of user) {
-    //         if (req.body.username !== ojb.username) {
-    //             count1++;
-    //         }
-    //     }
-    //     if (count1) {
-    //         res.status(400).send("Tên đăng nhập chưa chính xác!");
-    //         res.redirect("/login");
-    //     }
-
-    //     for (var ojb of user) {
-    //         if (req.body.password !== ojb.password) {
-    //             count2++;
-    //         }
-    //     }
-    //     if (count2) {
-    //         res.status(400).send('Mật khẩu chưa chính xác!');
-    //         res.redirect("/login");
-    //     }
-
-    //     res.cookie('name', 'admin', { expires: new Date(Date.now() + 9000000000) });
-    //     res.redirect("/");
-    // });
 };
