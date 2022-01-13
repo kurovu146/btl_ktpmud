316927b1b7a160759aadd18626330493fab3b2a0
Vũ Tuấn <kurobakaito146@gmail.com>
2022-01-13 15:11:23 +0700

    new version

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -75,16 +75,32 @@ controller.get("/stu_info", (req: Request, res: Response) => {
     })
 })
 
+controller.get("/room_info", (req: Request, res: Response) => {
+    const mysql = MySQLDb.getInstance();
+    const db = mysql.db;
+
+    db.query(`SELECT * FROM phieudk`, function(err, results) {
+        if (err) throw err;
+        const phieuDK = formatData(results)
+        res.render("room_info", { phieuDK })
+    })
+})
+
 function formatData(data: any[]) {
     const formattedData = data.map((i: any) => {
         const birth = new Date(i.NgaySinh);
         const NgayLapHD = new Date(i.NgayLapHD);
         const date = new Date(i.NgayNop);
+        const NgayBD = new Date(i.NgayBD);
+        const NgayLP = new Date(i.NgayLP);
+
         return {
             ...i,
             NgaySinh: birth.getFullYear()+'-' + (birth.getMonth()+1) + '-'+birth.getDate(),
             NgayLapHD: NgayLapHD.getFullYear()+'-' + (NgayLapHD.getMonth()+1) + '-'+NgayLapHD.getDate(),
             NgayNop: date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate(),
+            NgayBD: NgayBD.getFullYear()+'-' + (NgayBD.getMonth()+1) + '-'+NgayBD.getDate(),
+            NgayLP: NgayLP.getFullYear()+'-' + (NgayLP.getMonth()+1) + '-'+NgayLP.getDate(),
         }
     })
     return formattedData
